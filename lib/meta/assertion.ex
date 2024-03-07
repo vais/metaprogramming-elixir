7f36ef0380688016744f4f83cd0d6c4fc5517989
Vais Salikhov <vsalikhov@gmail.com>
2024-03-06 23:22:32 -0500

    Create a mini testing framework

--- lib/meta/assertion.ex
+++ lib/meta/assertion.ex
@@ -1,29 +1,73 @@
 defmodule Meta.Assertion do
-  defmacro expect({operator, _, [lhs, rhs]}) do
+  defmacro asserting({operator, _, [lhs, rhs]}) do
     quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
-      Meta.Assertion.Test.expect(operator, lhs, rhs)
+      Meta.Assertion.Test.asserting(operator, lhs, rhs)
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__)
+      @before_compile unquote(__MODULE__)
+
+      Module.register_attribute(__MODULE__, :tests, accumulate: true)
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def run(), do: Meta.Assertion.Test.run(@tests, __MODULE__)
+    end
+  end
+
+  defmacro testing(description, do: function_body) do
+    function_name = String.to_atom(description)
+
+    quote do
+      @tests {unquote(function_name), unquote(description)}
+      def unquote(function_name)(), do: unquote(function_body)
     end
   end
 
   defmodule Test do
-    def expect(:==, lhs, rhs) when lhs == rhs do
-      IO.write(".")
+    def run(tests, module) do
+      for {function, description} <- Enum.reverse(tests) do
+        case apply(module, function, []) do
+          :ok ->
+            IO.write(".")
+
+          {:fail, message} ->
+            IO.write("""
+
+            ===============================================
+            FAILURE: #{description}
+            ===============================================
+            #{message |> String.trim_trailing("\n")}
+            """)
+        end
+      end
+
+      IO.write("\n")
     end
 
-    def expect(:==, lhs, rhs) do
-      IO.puts("\nFAILURE:")
-      IO.puts("  Expected: #{lhs}")
-      IO.puts("  to be equal to: #{rhs}")
+    def asserting(:==, lhs, rhs) when lhs == rhs, do: :ok
+
+    def asserting(:==, lhs, rhs) do
+      {:fail,
+       """
+       Expected: #{lhs}
+       to be equal to: #{rhs}
+       """}
     end
 
-    def expect(:>, lhs, rhs) when lhs > rhs do
-      IO.write(".")
-    end
+    def asserting(:>, lhs, rhs) when lhs > rhs, do: :ok
 
-    def expect(:>, lhs, rhs) do
-      IO.puts("\nFAILURE:")
-      IO.puts("  Expected: #{lhs}")
-      IO.puts("  to be greater than: #{rhs}")
+    def asserting(:>, lhs, rhs) do
+      {:fail,
+       """
+       Expected: #{lhs}
+       to be greater than: #{rhs}
+       """}
     end
   end
 end
--- test/meta/assertion_test.exs
+++ test/meta/assertion_test.exs
@@ -1,35 +1,89 @@
 defmodule Meta.AssertionTest do
   use ExUnit.Case, async: true
 
-  alias Meta.Assertion
   alias ExUnit.CaptureIO
 
-  import Assertion
+  describe "asserting/1" do
+    import Meta.Assertion
 
-  describe "expect/1" do
-    test "==" do
-      assert CaptureIO.capture_io(fn ->
-               expect(7 == 7)
-               expect(3 == 3)
-               expect(1 == 5)
-             end) == """
+    test "equal" do
+      assert asserting(5 == 5) == :ok
+
+      assert asserting(1 == 5) ==
+               {:fail,
+                """
+                Expected: 1
+                to be equal to: 5
+                """}
+    end
+
+    test "greater than" do
+      assert asserting(5 > 1) == :ok
+
+      assert asserting(1 > 5) ==
+               {:fail,
+                """
+                Expected: 1
+                to be greater than: 5
+                """}
+    end
+  end
+
+  describe "testing/0" do
+    test "success" do
+      defmodule Success do
+        use Meta.Assertion
+
+        testing "integers can be added and subtracted" do
+          asserting(1 + 1 == 2)
+          asserting(2 + 3 == 5)
+          asserting(5 - 5 == 0)
+        end
+
+        testing "integers can be multiplied and divided" do
+          asserting(5 * 5 == 25)
+          asserting(10 / 2 == 5)
+        end
+      end
+
+      assert CaptureIO.capture_io(&Success.run/0) == """
              ..
-             FAILURE:
-               Expected: 1
-               to be equal to: 5
              """
     end
 
-    test ">" do
-      assert CaptureIO.capture_io(fn ->
-               expect(7 > 6)
-               expect(3 > 2)
-               expect(1 > 5)
-             end) == """
+    test "failure" do
+      defmodule Failure do
+        use Meta.Assertion
+
+        testing "integers can be added" do
+          asserting(1 + 1 == 2)
+          asserting(2 + 3 == 5)
+        end
+
+        testing "integers can be subtracted" do
+          asserting(15 - 5 == 10)
+          asserting(5 - 5 == 10)
+        end
+
+        testing "integers can be multiplied" do
+          asserting(5 * 5 == 25)
+          asserting(10 * 2 == 20)
+        end
+
+        testing "integers can be divided" do
+          asserting(5 / 5 == 1)
+          asserting(10 / 2 == 5)
+        end
+      end
+
+      assert CaptureIO.capture_io(&Failure.run/0) == """
+             .
+             ===============================================
+             FAILURE: integers can be subtracted
+             ===============================================
+             Expected: 0
+             to be equal to: 10
              ..
-             FAILURE:
-               Expected: 1
-               to be greater than: 5
              """
     end
   end
