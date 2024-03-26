69326cd95ffb3d548b81f374413519c61946a572
Vais Salikhov <vsalikhov@gmail.com>
2024-03-25 22:37:20 -0400

    Refactor Translator test to use "nested module" pattern

--- test/meta/translator_test.exs
+++ test/meta/translator_test.exs
@@ -1,7 +1,47 @@
-defmodule Meta.I18NTest do
+defmodule Meta.TranslatorTest do
   use ExUnit.Case, async: true
 
-  alias Meta.I18N
+  defmodule I18N do
+    use Meta.Translator
+
+    locale("en",
+      flash: [
+        hello: "Hello, %{first} %{last}!",
+        bye: "%{name}, Bye!"
+      ],
+      users: [
+        title: "Users",
+        address: [
+          city: "City",
+          something: [
+            else: [
+              the: [
+                thing: "the thing"
+              ]
+            ]
+          ]
+        ]
+      ],
+      title: [
+        users: {"user", "users"},
+        users_online:
+          {"There is 1 %{type} user online", "There are %{count} %{type} users online"}
+      ]
+    )
+
+    locale("fr",
+      flash: [
+        hello: "Salut, %{first} %{last}!",
+        bye: "%{name}, Au revoir!"
+      ],
+      users: [
+        title: "Utilisateurs",
+        address: [
+          city: "Le Citie"
+        ]
+      ]
+    )
+  end
 
   describe "t/3" do
     test "no translation" do
